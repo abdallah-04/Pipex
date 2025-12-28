53872563b0acc532c7b8d5388bfc6e3379d42b76
abdallah-04 <abdullah8almufleh@gmail.com>
2025-12-28 19:10:15 +0300

    add main

---
+++ fork.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+    // // return -1 if error and 0 for the chield and not 0 is for the main procrs
+    // // and the nunber of process i get in rach fork call is 2^n 
+    // int id = fork(); 
+    // if(id != 0)
+    // {
+    //     fork();
+    // }
+    // printf("id: %d\n", id);
+    // printf("hii\n");    
+
+    int id = fork();
+    int n;
+    if(id == 0)
+    {
+        n = 1;
+    }
+    else
+    {
+        n = 10; 
+    }
+}
